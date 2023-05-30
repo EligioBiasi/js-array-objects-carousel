07c1db7e3b550e2780261768fd0006886482f45a
Eligio Biasi <ex_98@live.it>
2023-05-30 16:18:08 +0200

    added structure for img in script

--- script.js
+++ script.js
@@ -31,5 +31,12 @@ const images = [
 // Milestone 1: Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
 // Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
 
+/*  */
+const jumboElement = document.querySelector('div.jumbo');
+
+jumboElement.innerHTML += 
+`<div class="img-container">
+  <img src="./img/" alt="">
+ </div>`
 // Milestone 2:
 // Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
